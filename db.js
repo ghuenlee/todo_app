af94a968c2837c05dfadd8158bb24406bfda940f
Xien Lee <kylesentient@gmail.com>
2016-03-16 23:21:18 +0100

    Added postgress addons

--- db.js
+++ db.js
@@ -1,8 +1,18 @@
 var sequelLib = require ('sequelize');
-var sequelInst = new sequelLib(undefined, undefined, undefined, {
-  'dialect': 'sqlite',
-  'storage': __dirname + '/data/dev-todo-app.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelInst;
+
+if ( env == 'production'){
+  sequelInst = new sequelLib(process.env.DATABASE_URL, {
+    dialect: 'postgres'
+  });
+} else {
+  sequelInst = new sequelLib(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/data/dev-todo-app.sqlite'
+  });
+}
+
 
 var db = {};
 
